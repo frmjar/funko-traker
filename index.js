4e0f7fa47fa87a78a09914c65e6513db691d8cd3
Felix Redondo <frmanzanares@hotmail.com>
2024-08-05 02:30:08 +0200

    chore: Refactor code to improve readability and maintainability

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
 import puppeteer from 'puppeteer';
 
-const funkos = async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com/search?q=Funko+Pop+Harry+Potter+Buckbeak+123');
+const openBrowser = async () => {
+    const browser = await puppeteer.launch({ headless: true, timeout: 60000 });
+    return { browser };
+}
 
-    const funkos = await page.evaluate(() => {
-        const funkos = [];
-
-        document.querySelectorAll('.top-pla-group-inner .pla-unit').forEach(funko => {
-            const web = funko.getAttribute('data-dtld');
-            const image = funko.querySelector('.pla-unit-container .pla-unit-img-container img')?.getAttribute('src');
-            const link = funko.querySelector('.pla-unit-container a.pla-unit-img-container-link')?.getAttribute('href');
-            const name = funko.querySelector('.pla-unit-container .pla-unit-title span')?.innerText;
-            const price = funko.querySelector('.pla-unit-container .pla-unit-title')?.nextElementSibling.querySelector('span')?.innerText;
-            const shipping = funko.querySelector('.pla-unit-container .pla-extensions-container >div')?.innerText;
-            const stars = funko.querySelector('.pla-unit-container .pla-extensions-container >div')?.nextElementSibling?.querySelector('span > span')?.getAttribute('aria-label').replace(/,\s*$/, '');
-            const valoraciones = funko.querySelector('.pla-unit-container .pla-extensions-container >div')?.nextElementSibling?.querySelector('span')?.nextElementSibling?.innerText?.replace(/\(|\)/g, '');
-
-            funkos.push({web, image: 'image', link, name, price, shipping, stars, valoraciones });
-        });
-        
-        return funkos;
-    });
+const closeBrowser = async (browser) => {
     await browser.close();
-    return funkos;
+}
 
+const extraerDatos = () => {
+    const funkos = [];
+    document.querySelectorAll('.pla-unit').forEach(funko => {
+        const web = funko.getAttribute('data-dtld');
+        const image = funko.querySelector('.pla-unit-container .pla-unit-img-container img')?.getAttribute('src');
+        const link = funko.querySelector('.pla-unit-container a.pla-unit-img-container-link')?.getAttribute('href');
+        const name = funko.querySelector('.pla-unit-container .pla-unit-title span')?.innerText;
+        const price = funko.querySelector('.pla-unit-container .pla-unit-title')?.nextElementSibling?.querySelector('span')?.innerText;
+        const shipping = funko.querySelector('.pla-unit-container .pla-extensions-container > div')?.innerText;
+        const stars = funko.querySelector('.pla-unit-container .pla-extensions-container > div')?.nextElementSibling?.querySelector('span > span')?.getAttribute('aria-label')?.replace(/,\s*$/, '');
+        const valoraciones = funko.querySelector('.pla-unit-container .pla-extensions-container > div')?.nextElementSibling?.querySelector('span')?.nextElementSibling?.innerText?.replace(/\(|\)/g, '');
+
+        funkos.push({ web, image:'image', link, name, price, shipping, stars, valoraciones });
+    });
+    return funkos;
+}
+
+const funkos = async (browser) => {
+    const page = await browser.newPage();
+
+    try {
+        await page.goto('https://www.google.com/search?q=funko+queen+historia+1170', { waitUntil: 'domcontentloaded' });
+        const funkosList = await page.evaluate(extraerDatos);
+
+        await page.close();
+        return funkosList;
+    } catch (error) {
+        console.error('Error durante la extracción de datos:', error);
+        await page.close();
+        return [];
+    }
+}
+
+(async () => {
+    const { browser } = await openBrowser();
+    const funkosValues = new Map();
+
+    try {
+        await Promise.all(Array(5).fill().map(async () => {
+            const funkoList = await funkos(browser);
+            funkoList.forEach(f => funkosValues.set(f.name, f));
+        }));
+    } catch (error) {
+        console.error('Error en la ejecución principal:', error);
+    } finally {
+        await closeBrowser(browser);
     }
 
-funkos().then(console.log);
+    funkosValues.forEach(f => console.log(f));
+})();
