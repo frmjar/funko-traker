c3cfc069f576c06c1a7e6c30f1469ab6643f4824
Félix Redondo <frmanzanares@hotmail.com>
2024-08-06 13:56:01 +0200

    feat: Refactor Puppeteer class and improve funkos function
    The code changes introduce a new Puppeteer class and refactor the index file. Additionally, the funkos function now accepts a search parameter, allowing users to search for specific funkos. This improves the functionality and flexibility of the application.

--- Puppeteer.js
+++ Puppeteer.js
@@ -33,7 +33,53 @@ export default class Puppeteer {
         const stars = funko.querySelector('.pla-unit-container .pla-extensions-container > div')?.nextElementSibling?.querySelector('span > span')?.getAttribute('aria-label')?.replace(/,\s*$/, '')
         const valoraciones = funko.querySelector('.pla-unit-container .pla-extensions-container > div')?.nextElementSibling?.querySelector('span')?.nextElementSibling?.innerText?.replace(/\(|\)/g, '')
 
-        funkos.push({ web, image: 'image', link, name, price, shipping, stars, valoraciones })
+        funkos.push({ web, image, link, name, price, shipping, stars, valoraciones })
+      })
+
+      return funkos
+    })
+
+    return funkosList
+  }
+
+  async evaluatePatrocinadosLateral (page) {
+    const funkosList = await page.evaluate(() => {
+      const funkos = []
+
+      document.querySelectorAll('div[data-attrid="kc:/shopping/gpc:organic-offers"] div[role="listitem"]').forEach(funko => {
+        const funkoBox = funko.querySelectorAll('a')[0]
+        const textos = funkoBox.innerText.split('\n')
+        const indexNameNormal = !!textos[2]?.toLowerCase().includes('funko') || !!textos[2]?.toLowerCase().includes('pop')
+
+        const web = textos[0]
+        const link = funkoBox.getAttribute('href')
+        const name = indexNameNormal ? textos[2] : textos[3]
+        const price = textos[1]
+        const shipping = indexNameNormal ? textos[4] : null
+
+        funkos.push({ web, link, name, price, shipping })
+      })
+
+      return funkos
+    })
+
+    return funkosList
+  }
+
+  async evaluatePrincipal (page) {
+    const funkosList = await page.evaluate(() => {
+      const funkos = []
+
+      document.querySelectorAll('div[data-async-context] > div').forEach(funko => {
+        const web = funko.querySelectorAll('div.notranslate span') ? funko.querySelectorAll('div.notranslate span')[1]?.innerText : null
+        let image = funko.querySelectorAll('a img') ? funko.querySelectorAll('a img')[1]?.getAttribute('src') : null
+        image = image === 'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==' ? null : image
+
+        const link = funko.querySelector('a')?.getAttribute('href')
+        const name = funko.querySelector('h3')?.innerText
+        const price = funko.querySelector('div.ChPIuf > span')?.innerText
+
+        funkos.push({ web, image, link, name, price })
       })
 
       return funkos
--- index.js
+++ index.js
@@ -7,15 +7,31 @@ await browser.init()
 const funkosValues = new Map()
 const funkosDescartados = new Map()
 
+const organiceFunkos = (funkosList, funkosValues, funkosDescartados) => {
+  funkosList.forEach(f => {
+    if (f.web?.toLowerCase().includes('idealo') || f.web?.toLowerCase().includes('globerada')) return
+
+    if (search.toLowerCase().split(' ').some(s => f.name?.toLowerCase().includes(s.toLowerCase()))) {
+      funkosValues.set(f.name, f)
+    } else {
+      funkosDescartados.set(f.name, f)
+    }
+  })
+}
+
 try {
   await Promise.all(Array(5).fill().map(async () => {
     const page = await browser.newPage(search)
-    const funkoList = await browser.evaluatePatrocinadosSuperior(page)
+
+    const funkoLists = await Promise.all([
+      browser.evaluatePatrocinadosSuperior(page),
+      browser.evaluatePatrocinadosLateral(page),
+      browser.evaluatePrincipal(page)
+    ])
+
     await browser.closePage(page)
 
-    funkoList.forEach(f => {
-      if (search.toLowerCase().split(' ').some(s => f.name?.toLowerCase().includes(s.toLowerCase()))) { funkosValues.set(f.name, f) } else { funkosDescartados.set(f.name, f) }
-    })
+    funkoLists.forEach(funkoList => organiceFunkos(funkoList, funkosValues, funkosDescartados))
   }))
 } catch (error) {
   console.error('Error en la ejecución principal:', error)
@@ -24,5 +40,5 @@ try {
 }
 
 funkosValues.forEach(f => console.log(f))
-console.log('Descartados:', funkosDescartados.size)
+console.log('Descartados:')
 funkosDescartados.forEach(f => console.log(f))
